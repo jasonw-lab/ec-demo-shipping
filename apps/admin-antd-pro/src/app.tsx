f74f63cb68d2411e2730cf93bbb01272ab7dfe4b
jason.w <jasonw.lab@gmail.com>
2026-02-02 11:33:44 +0900

    fix(admin-antd-pro): remove hardcoded baseURL to use proxy

--- apps/admin-antd-pro/src/app.tsx
+++ apps/admin-antd-pro/src/app.tsx
@@ -152,6 +152,5 @@ export const layout: RunTimeLayoutConfig = ({
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request: RequestConfig = {
-  baseURL: 'https://proapi.azurewebsites.net',
   ...errorConfig,
 };
